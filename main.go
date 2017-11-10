03c4a48cef42addd09b6f7731cee0826ae36a658
Brian Wigginton <brianwigginton@gmail.com>
2017-11-09 18:14:18 -0600

    refactor of nexrad processing

--- main.go
+++ main.go
@@ -7,227 +7,162 @@ import (
 	"io"
 	"os"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/davecgh/go-spew/spew"
 )
 
-// VolumeHeaderRecord for NEXRAD Archive II Data Streams
-//
-// Description:
-// The Volume Header Record
-// The Volume Header Record is fixed length and contains information uniquely
-// identifying the format and the data that follows. Sits at the beginning of the
-// Archive II data stream.
-//
-// Volume Header Record Data Format:
-// The first 9 bytes is a character constant of which the last 2 characters
-// identify the version. The next 3 bytes is a numeric string field starting
-// with the value 001 and increasing by one for each volume of radar data in the
-// queue to a maximum value of 999. Once the maximum value is reached the value
-// will be rolled over. The combined 12 bytes are called the Archive II filename.
-// The next 4 bytes contain the NEXRAD-modified Julian date the volume was
-// produced at the RDA followed by 4 bytes containing the time the volume was
-// recorded. The date and time integer values are big Endian. The last 4 bytes
-// contain a 4-letter radar identifier assigned by ICAO.
-//
-// Version Number Reference:
-// Version 02: Super Resolution disabled at the RDA (pre RDA Build 12.0)
-// Version 03: Super Resolution (pre RDA Build 12.0)
-// Version 04: Recombined Super Resolution
-// Version 05: Super Resolution disabled at the RDA (RDA Build 12.0 and later)
-// Version 06: Super Resolution (RDA Build 12.0 and later)
-// Version 07: Recombined Super Resolution (RDA Build 12.0 and later)
-type VolumeHeaderRecord struct {
-	Tape      [7]byte
-	Version   [2]byte
-	Extension [3]byte
-	// Date NEXRAD- modified Julian
-	Date [4]byte
-	// Time ms since midnight
-	Time [4]byte
-	// ICAO Radar identifier in ASCII. The four uppercase character International Civil Aviation Organization identifier of the radar producing the data.
-	ICAO [4]byte
-}
-
-// LDMRecord (Local Data Manager) contains NEXRAD message data.
-// Following the Volume Header Record are variable-length records containing the
-// Archive II data messages. These records are referred to as LDM Compressed Record(s).
-type LDMRecord struct {
-	Size           uint32
-	MetaDataRecord []byte
-}
-
-type MessageHeader struct {
-	MessageSize         uint16
-	RDARedundantChannel uint8
-	MessageType         uint8
-	IDSequenceNumber    uint16
-	JulianDate          uint16
-	MillisOfDay         uint32
-	NumMessageSegments  uint16
-	MessageSegmentNum   uint16
-}
-
-// Message31 Digital Radar Data Generic Format
-//
-// Description:
-// The message consists of base data information, that is, reflectivity, mean
-// radial velocity, spectrum width, differential reflectivity, differential
-// phase, correlation coefficient, azimuth angle, elevation angle, cut type,
-// scanning strategy and calibration parameters. The frequency and volume of the
-// message will be dependent on the scanning strategy and the type of data
-// associated with that scanning strategy.
-type Message31 struct {
-	Header Message31Header
-}
-
-type Message31Header struct {
-	RadarIdentifier          [4]byte
-	CollectionTime           uint32
-	ModifiedJulianDate       uint16
-	AzimuthNumber            uint16
-	AzimuthAngle             float32
-	CompressionIndicator     uint8
-	Spare                    uint8
-	RadialLength             uint16
-	AzimuthResolutionSpacing uint8
-	RadialStatus             uint8
-	ElevationNumber          uint8
-	CutSectorNumber          uint8
-	ElevationAngle           float32
-	RadialSpotBlankingStatus uint8
-	AzimuthIndexingMode      uint8
-	DataBlockCount           uint16
-	DataBlockPointers        [9]uint32
-}
-
-type DataBlock struct {
-	DataBlockType [1]byte
-	DataName      [3]byte
-}
-
-type VolumeData struct {
-	DataBlock
-	LRTUP                          uint16
-	VersionMajor                   uint8
-	VersionMinor                   uint8
-	Lat                            float32
-	Long                           float32
-	SiteHeight                     uint16
-	FeedhornHeight                 uint16
-	CalibrationConstant            float32
-	SHVTXPowerHor                  float32
-	SHVTXPowerVer                  float32
-	SystemDifferentialReflectivity float32
-	InitialSystemDifferentialPhase float32
-	VolumeCoveragePatternNumber    uint16
-	ProcessingStatus               uint16
-}
-
-type ElevationData struct {
-	DataBlock
-	LRTUP      uint16
-	ATMOS      [2]byte
-	CalibConst float32
-}
-
-type RadialData struct {
-	DataBlock
-	LRTUP              uint16
-	UnambiguousRange   uint16
-	NoiseLevelHorz     float32
-	NoiseLevelVert     float32
-	NyquistVelocity    uint16
-	Spares             [2]byte
-	CalibConstHorzChan float32
-	CalibConstVertChan float32
-}
-
-type GenericDataMoment struct {
-	DataBlock
-	Reserved                      uint32
-	NumberDataMomentGates         uint16
-	DataMomentRange               uint16
-	DataMomentRangeSampleInterval uint16
-	TOVER                         uint16
-	SNRThreshold                  uint16
-	ControlFlags                  [1]byte
-	DataWordSize                  uint8
-	Scale                         float32
-	Offset                        float32
-}
-
-// RefData Reflectivity
-type DataMoment struct {
-	GenericDataMoment
-	Data []byte
-}
-
 func main() {
 
-	f, _ := os.Open("KEWX20170826_042554_V06")
+	logrus.SetLevel(logrus.DebugLevel)
+
+	// nrl2 := NEXRADLevel2File{
+	// 	VolData: []VolumeData{},
+	// 	ElvData: []ElevationData{},
+	// 	RadData: []RadialData{},
+	// 	Moments: make(map[string][]DataMoment),
+	// }
+
+	f, _ := os.Open("data/KCRP20170826_235827_V06")
+	// f, _ := os.Open("data/KGRK20170827_234611_V06")
+
 	vhr := VolumeHeaderRecord{}
+	binary.Read(f, binary.BigEndian, &vhr)
 
-	binary.Read(f, binary.LittleEndian, &vhr)
-
-	// ==============================
-	// LDM Compressed Metadata Record
-	// ==============================
-
+	// First record consists of Metadata message types 15, 13, 18, 3, 5, and 2
 	ldm := LDMRecord{}
 	binary.Read(f, binary.BigEndian, &ldm.Size)
+	decompress(f, ldm.Size)
 
-	compressedData := make([]byte, ldm.Size)
+	for true {
+		logrus.Debugf("-------------------- LDM RECORD START --------------------")
+		// preview(f, 32)
+		ldm := LDMRecord{}
+
+		if err := binary.Read(f, binary.BigEndian, &ldm.Size); err != nil {
+			if err == io.EOF {
+				logrus.Info("completed processing")
+				break
+			}
+			logrus.Panic(err.Error())
+		}
+		logrus.Debugf("decompressing (%12d bytes)", ldm.Size)
+		// preview(f, 16)
+
+		msgBuf := decompress(f, ldm.Size)
+
+		for true {
+			msgBuf.Seek(12, io.SeekCurrent)
+			header := MessageHeader{}
+			if err := binary.Read(msgBuf, binary.BigEndian, &header); err != nil {
+				if err != io.EOF {
+					logrus.Panic(err.Error())
+				}
+				break
+			}
+			// if header.MessageType != 31 {
+			logrus.Infof("=== Message %d", header.MessageType)
+			// }
+			// spew.Dump(header)
+			switch header.MessageType {
+			case 0:
+				spew.Dump(header)
+				msg := make([]byte, header.MessageSize)
+				binary.Read(msgBuf, binary.BigEndian, &msg)
+				spew.Dump(msg)
+				return
+			case 31:
+				m31 := msg31(msgBuf)
+				if m31.Header.ElevationNumber == 1 {
+					logrus.Infof("\tAzimuth Number: %d", m31.Header.AzimuthNumber)
+					logrus.Infof("\tAzimuth Angle: %f", m31.Header.AzimuthAngle)
+					logrus.Infof("\tAzimuth Res Spacing: %d", m31.Header.AzimuthResolutionSpacing)
+					logrus.Infof("\tElevation Angle: %f", m31.Header.ElevationAngle)
+					logrus.Infof("\tElevation Number: %d", m31.Header.ElevationNumber)
+					logrus.Infof("\tRadialStatus: %d", m31.Header.RadialStatus)
+					logrus.Infof("\tRadial Length: %d", m31.Header.RadialLength)
+					logrus.Infof("\tCut Sector Num: %d", m31.Header.CutSectorNumber)
+				}
+
+				dm := m31.MomentData[3].(DataMoment)
+				spew.Dump(dm)
+				for _, n := range dm.Data {
+					spew.Dump((float32(n) - dm.Offset) / dm.Scale)
+				}
+				return
+			case 2:
+				// spew.Dump(header)
+				m2 := RDAStatusMessage2{}
+				binary.Read(msgBuf, binary.BigEndian, &m2)
+				// spew.Dump(m2)
+				// eat the rest of the record since we know it's 2432 bytes
+				msg := make([]byte, 2432-16-54-12)
+				binary.Read(msgBuf, binary.BigEndian, &msg)
+				// spew.Dump(msg)
+			default:
+				spew.Dump(header)
+				// eat the rest of the record since we know it's 2432 bytes (2416 - header)
+				msg := make([]byte, 2416)
+				binary.Read(msgBuf, binary.BigEndian, &msg)
+				spew.Dump(msg)
+			}
+		}
+	}
+}
+
+func preview(r io.ReadSeeker, n int) {
+	preview := make([]byte, n)
+	binary.Read(r, binary.BigEndian, &preview)
+	spew.Dump(preview)
+	r.Seek(-int64(n), io.SeekCurrent)
+}
+
+func decompress(f *os.File, size uint32) *bytes.Reader {
+	compressedData := make([]byte, size)
 	binary.Read(f, binary.BigEndian, &compressedData)
-
 	bz2Reader := bzip2.NewReader(bytes.NewReader(compressedData))
+	extractedData := bytes.NewBuffer([]byte{})
+	io.Copy(extractedData, bz2Reader)
+	return bytes.NewReader(extractedData.Bytes())
+}
 
-	meta := bytes.NewBuffer([]byte{})
-	io.Copy(meta, bz2Reader)
-	ldm.MetaDataRecord = meta.Bytes()
-
-	messages := make([][2432]byte, 134)
-	binary.Read(meta, binary.BigEndian, &messages)
-
-	// ==============================
-	// Message 31 Data
-	// ==============================
-
-	var message31Size uint32
-	binary.Read(f, binary.BigEndian, &message31Size)
-	m31Buf := decomp(f, message31Size)
-	m31Reader := bytes.NewReader(m31Buf)
-	m31Reader.Seek(12, io.SeekCurrent) // eat 12 empty bytes
-
-	// header
-
-	header := MessageHeader{}
-	binary.Read(m31Reader, binary.BigEndian, &header)
-
+func msg31(r *bytes.Reader) *Message31 {
 	m31h := Message31Header{}
-	binary.Read(m31Reader, binary.BigEndian, &m31h)
+	binary.Read(r, binary.BigEndian, &m31h)
+
+	m31 := Message31{
+		Header:     m31h,
+		MomentData: []interface{}{},
+	}
 
 	for i := uint16(0); i < m31h.DataBlockCount; i++ {
 		d := DataBlock{}
-		binary.Read(m31Reader, binary.BigEndian, &d)
-		m31Reader.Seek(-4, io.SeekCurrent)
-		switch string(d.DataName[:]) {
+		if err := binary.Read(r, binary.BigEndian, &d); err != nil {
+			logrus.Panic(err.Error())
+		}
+		r.Seek(-4, io.SeekCurrent)
+
+		// spew.Dump(d)
+
+		blockName := string(d.DataName[:])
+		// fmt.Printf("\t%s\n", blockName)
+		switch blockName {
 		case "VOL":
-			vd := VolumeData{}
-			binary.Read(m31Reader, binary.BigEndian, &vd)
-			spew.Dump(vd)
+			d := VolumeData{}
+			binary.Read(r, binary.BigEndian, &d)
+			m31.MomentData = append(m31.MomentData, d)
 		case "ELV":
-			ed := ElevationData{}
-			binary.Read(m31Reader, binary.BigEndian, &ed)
-			spew.Dump(ed)
+			d := ElevationData{}
+			binary.Read(r, binary.BigEndian, &d)
+			m31.MomentData = append(m31.MomentData, d)
 		case "RAD":
-			rad := RadialData{}
-			binary.Read(m31Reader, binary.BigEndian, &rad)
-			spew.Dump(rad)
+			d := RadialData{}
+			binary.Read(r, binary.BigEndian, &d)
+			m31.MomentData = append(m31.MomentData, d)
 		case "REF":
 			fallthrough
 		case "VEL":
 			fallthrough
-		case "SW":
+		case "SW ":
 			fallthrough
 		case "ZDR":
 			fallthrough
@@ -235,54 +170,20 @@ func main() {
 			fallthrough
 		case "RHO":
 			m := GenericDataMoment{}
-			binary.Read(m31Reader, binary.BigEndian, &m)
+			binary.Read(r, binary.BigEndian, &m)
 			//LDM is the amount of space in bytes required for a data moment array and equals
 			//((NG * DWS) / 8) where NG is the number of gates at the gate spacing resolution specified and DWS is the number of bits stored for each gate (DWS is always a multiple of 8).
 			ldm := m.NumberDataMomentGates * uint16(m.DataWordSize) / 8
 			data := make([]uint8, ldm)
-			binary.Read(m31Reader, binary.BigEndian, &data)
-			spew.Dump(data)
-			// dm := DataMoment{
-			// 	GenericDataMoment: m,
-			// 	Data:              data,
-			// }
-
-			// spew.Dump(dm)
+			binary.Read(r, binary.BigEndian, &data)
+			d := DataMoment{
+				GenericDataMoment: m,
+				Data:              data,
+			}
+			m31.MomentData = append(m31.MomentData, d)
+		default:
+			logrus.Panicf("Data Block - unknown type '%s'", blockName)
 		}
 	}
-
-	// data blocks
-
-	// volume := VolumeData{}
-	// binary.Read(m31Reader, binary.BigEndian, &volume)
-
-	// elev := ElevationData{}
-	// binary.Read(m31Reader, binary.BigEndian, &elev)
-
-	// radial := RadialData{}
-	// binary.Read(m31Reader, binary.BigEndian, &radial)
-
-	// ref := RefData{
-	// 	Data: make([byte], )
-	// }
-	// binary.Read(m31Reader, binary.BigEndian, &ref)
-	// spew.Dump(ref)
-
-	// vel := VelData{}
-	// binary.Read(m31Reader, binary.BigEndian, &vel)
-
-	// spew.Dump(vel)
-}
-
-func decomp(f *os.File, size uint32) []byte {
-	cdata := make([]byte, size)
-	binary.Read(f, binary.BigEndian, &cdata)
-	bz2Reader := bzip2.NewReader(bytes.NewReader(cdata))
-	ddata := bytes.NewBuffer([]byte{})
-	io.Copy(ddata, bz2Reader)
-	return ddata.Bytes()
-}
-
-func message31Header() Message31Header {
-	return Message31Header{}
+	return &m31
 }
---
+++ types.go
@@ -0,0 +1,227 @@
+package main
+
+type NEXRADLevel2File struct {
+	VolData []VolumeData
+	ElvData []ElevationData
+	RadData []RadialData
+	Moments map[string][]DataMoment
+}
+
+// VolumeHeaderRecord for NEXRAD Archive II Data Streams
+//
+// Description:
+// The Volume Header Record
+// The Volume Header Record is fixed length and contains information uniquely
+// identifying the format and the data that follows. Sits at the beginning of the
+// Archive II data stream.
+//
+// Volume Header Record Data Format:
+// The first 9 bytes is a character constant of which the last 2 characters
+// identify the version. The next 3 bytes is a numeric string field starting
+// with the value 001 and increasing by one for each volume of radar data in the
+// queue to a maximum value of 999. Once the maximum value is reached the value
+// will be rolled over. The combined 12 bytes are called the Archive II filename.
+// The next 4 bytes contain the NEXRAD-modified Julian date the volume was
+// produced at the RDA followed by 4 bytes containing the time the volume was
+// recorded. The date and time integer values are big Endian. The last 4 bytes
+// contain a 4-letter radar identifier assigned by ICAO.
+//
+// Version Number Reference:
+// Version 02: Super Resolution disabled at the RDA (pre RDA Build 12.0)
+// Version 03: Super Resolution (pre RDA Build 12.0)
+// Version 04: Recombined Super Resolution
+// Version 05: Super Resolution disabled at the RDA (RDA Build 12.0 and later)
+// Version 06: Super Resolution (RDA Build 12.0 and later)
+// Version 07: Recombined Super Resolution (RDA Build 12.0 and later)
+type VolumeHeaderRecord struct {
+	Tape      [7]byte
+	Version   [2]byte
+	Extension [3]byte
+	// Date NEXRAD- modified Julian
+	Date [4]byte
+	// Time ms since midnight
+	Time [4]byte
+	// ICAO Radar identifier in ASCII. The four uppercase character International Civil Aviation Organization identifier of the radar producing the data.
+	ICAO [4]byte
+}
+
+// LDMRecord (Local Data Manager) contains NEXRAD message data.
+// Following the Volume Header Record are variable-length records containing the
+// Archive II data messages. These records are referred to as LDM Compressed Record(s).
+type LDMRecord struct {
+	Size           uint32
+	MetaDataRecord []byte
+}
+
+type MessageHeader struct {
+	MessageSize         uint16
+	RDARedundantChannel uint8
+	MessageType         uint8
+	IDSequenceNumber    uint16
+	JulianDate          uint16
+	MillisOfDay         uint32
+	NumMessageSegments  uint16
+	MessageSegmentNum   uint16
+}
+
+// Message31 Digital Radar Data Generic Format
+//
+// Description:
+// The message consists of base data information, that is, reflectivity, mean
+// radial velocity, spectrum width, differential reflectivity, differential
+// phase, correlation coefficient, azimuth angle, elevation angle, cut type,
+// scanning strategy and calibration parameters. The frequency and volume of the
+// message will be dependent on the scanning strategy and the type of data
+// associated with that scanning strategy.
+type Message31 struct {
+	Header     Message31Header
+	MomentData []interface{}
+}
+
+// RadialStatus enumerations
+// 00: Start of new Elevation
+// 01: Intermediate Radial Data
+// 02: End of Elevation
+// 03: Beginning of Volume Scan
+// 04: End of Volume Scan
+// 05: Start of new Elevation - Last Elevation in VCP
+
+type RDAStatusMessage2 struct {
+	RDAStatus                       uint16
+	OperabilityStatus               uint16
+	ControlStatus                   uint16
+	AuxPowerGeneratorState          uint16
+	AvgTxPower                      uint16
+	HorizRefCalibCorr               uint16
+	DataTxEnabled                   uint16
+	VolumeCoveragePatternNum        uint16
+	RDAControlAuth                  uint16
+	RDABuild                        uint16
+	OperationalMode                 uint16
+	SuperResStatus                  uint16
+	ClutterMitigationDecisionStatus uint16
+	AvsetStatus                     uint16
+	RDAAlarmSummary                 uint16
+	CommandAck                      uint16
+	ChannelControlStatus            uint16
+	SpotBlankingStatus              uint16
+	BypassMapGenDate                uint16
+	BypassMapGenTime                uint16
+	ClutterFilterMapGenDate         uint16
+	ClutterFilterMapGenTime         uint16
+	VertRefCalibCorr                uint16
+	TransitionPwrSourceStatus       uint16
+	RMSControlStatus                uint16
+	PerformanceCheckStatus          uint16
+	AlarmCodes                      uint16
+}
+
+type Message31Header struct {
+	RadarIdentifier [4]byte
+	// CollectionTime Radial data collection time in milliseconds past midnight GMT
+	CollectionTime uint32
+	// ModifiedJulianDate Current Julian date - 2440586.5
+	ModifiedJulianDate uint16
+	// AzimuthNumber Radial number within elevation scan
+	AzimuthNumber uint16
+	// AzimuthAngle Azimuth angle at which radial data was collected
+	AzimuthAngle float32
+	// CompressionIndicator Indicates if message type 31 is compressed and what method of compression is used. The Data Header Block is not compressed.
+	CompressionIndicator uint8
+	Spare                uint8
+	// RadialLength Uncompressed length of the radial in bytes including the Data Header block length
+	RadialLength uint16
+	// AzimuthResolutionSpacing Azimuthal spacing between adjacent radials
+	AzimuthResolutionSpacing uint8
+	// RadialStatus Radial Status
+	RadialStatus uint8
+	// ElevationNumber Elevation number within volume scan
+	ElevationNumber uint8
+	// CutSectorNumber Sector Number within cut
+	CutSectorNumber uint8
+	// ElevationAngle Elevation angle at which radial radar data was collected
+	ElevationAngle float32
+	// RadialSpotBlankingStatus Spot blanking status for current radial, elevation scan and volume scan
+	RadialSpotBlankingStatus uint8
+	// AzimuthIndexingMode Azimuth indexing value (Set if azimuth angle is keyed to constant angles)
+	AzimuthIndexingMode uint8
+	DataBlockCount      uint16
+	DataBlockPointers   [9]uint32
+}
+
+type DataBlock struct {
+	DataBlockType [1]byte
+	DataName      [3]byte
+}
+
+type VolumeData struct {
+	DataBlock
+	// LRTUP Size of data block in bytes
+	LRTUP                          uint16
+	VersionMajor                   uint8
+	VersionMinor                   uint8
+	Lat                            float32
+	Long                           float32
+	SiteHeight                     uint16
+	FeedhornHeight                 uint16
+	CalibrationConstant            float32
+	SHVTXPowerHor                  float32
+	SHVTXPowerVer                  float32
+	SystemDifferentialReflectivity float32
+	InitialSystemDifferentialPhase float32
+	VolumeCoveragePatternNumber    uint16
+	ProcessingStatus               uint16
+}
+
+type ElevationData struct {
+	DataBlock
+	// LRTUP Size of data block in bytes
+	LRTUP uint16
+	// ATMOS Atmospheric Attenuation Factor
+	ATMOS [2]byte
+	// CalibConst Scaling constant used by the Signal Processor for this elevation to calculate reflectivity
+	CalibConst float32
+}
+
+type RadialData struct {
+	DataBlock
+	// LRTUP Size of data block in bytes
+	LRTUP uint16
+	// UnambiguousRange, Interval Size
+	UnambiguousRange   uint16
+	NoiseLevelHorz     float32
+	NoiseLevelVert     float32
+	NyquistVelocity    uint16
+	Spares             [2]byte
+	CalibConstHorzChan float32
+	CalibConstVertChan float32
+}
+
+type GenericDataMoment struct {
+	DataBlock
+	Reserved uint32
+	// NumberDataMomentGates Number of data moment gates for current radial
+	NumberDataMomentGates uint16
+	// DataMomentRange Range to center of first range gate
+	DataMomentRange uint16
+	// DataMomentRangeSampleInterval Size of data moment sample interval
+	DataMomentRangeSampleInterval uint16
+	// TOVER Threshold parameter which specifies the minimum difference in echo power between two resolution gates for them not to be labeled "overlayed"
+	TOVER uint16
+	// SNRThreshold SNR threshold for valid data
+	SNRThreshold uint16
+	// ControlFlags Indicates special control features
+	ControlFlags uint8
+	// DataWordSize Number of bits (DWS) used for storing data for each Data Moment gate
+	DataWordSize uint8
+	// Scale value used to convert Data Moments from integer to floating point data
+	Scale float32
+	// Offset value used to convert Data Moments from integer to floating point data
+	Offset float32
+}
+
+// RefData Reflectivity
+type DataMoment struct {
+	GenericDataMoment
+	Data []byte
+}
